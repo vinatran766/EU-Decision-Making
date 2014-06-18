108a8f0ae7fecdf59de0f37709624c2740be5025
Richard Bartlett <rich@loomio.org>
2014-06-18 12:06:52 +1200

    styles

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -3,12 +3,14 @@ class WallController < BaseController
   def show
     # discussions that had activity in the last 24 hours
 
+
+    # TODO UNDO CHANGES TO THIS FILE
     groups = current_user.inbox_groups
-    @time_since = 3.day.ago
+    @time_since = 11.days.ago
 
     @discussions_by_group = Queries::VisibleDiscussions.new(user: current_user,
                                                             groups: groups).
-                                                            unread.
+                                                            # unread.
                                                             active_since(@time_since).
                                                             group_by(&:group)
 
